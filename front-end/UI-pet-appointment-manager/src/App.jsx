42fd8eedde030018791cabd1d29a398b28314100
MagicExist <johhanparra1@gmail.com>
2025-06-18 11:08:44 -0500

    (front-end): add initial setup

---
+++ front-end/UI-pet-appointment-manager/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import AppointmentView from './components/AppointmentsView'
+
+function App() {
+
+  return (
+    <>
+      <AppointmentView></AppointmentView>
+    </>
+  )
+}
+
+export default App
